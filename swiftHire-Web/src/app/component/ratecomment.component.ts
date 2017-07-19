3c601305f26abd75ef758bac4e1fb60deb6addca
ChandaraLeang <leangchandara@gmail.com>
2017-07-18 22:21:55 -0500

    added ratecomment.component.html, ratecomment.component.ts

--- swiftHire-Web/src/app/component/ratecomment.component.ts
+++ swiftHire-Web/src/app/component/ratecomment.component.ts
@@ -5,28 +5,37 @@ import { Component, OnInit, Inject,OnDestroy,Input } from '@angular/core';
 import { JobService } from "app/service/job.service";
 import { UserService } from "app/service/user.service";
 import { Subscription } from "rxjs/Rx";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
   selector: 'ratecomment',
-  template:`
-   <div>Show details info for Job </div>
-    <div>Show one form </div>
-    {{jobObj | json}}
-  `,
+  templateUrl:`./ratecomment.component.html`,
   providers:[]
 })
 
 export class RateCommentComponent implements OnDestroy {
   private subscription: Subscription;
+  myForm: FormGroup;
 
   @Input() jobObj:any;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService) {}
 
+  ngOnInit() {
+    let da = new Date().toString();
+    this.subscription = this.userService.addCommentForOwner(content, da).subscribe(resp=>{
+        this.jobObj=resp;
+      },
+      error=>{
+        console.log('This doesnt work');
+      },()=>{});
   }
 
+  onSubmit() {
+    console.log(this.myForm);
+  }
 
   ngOnDestroy() {
-    //this.subscription.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
