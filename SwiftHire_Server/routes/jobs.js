8e7ee4c0c2ff7ac96b45f6f1c0d48985c7245a34
Samuel.Cai <samuel.cai@ceosoftcenters.com>
2017-07-18 20:15:32 -0500

    Add some code for posted jobs list.

--- SwiftHire_Server/routes/jobs.js
+++ SwiftHire_Server/routes/jobs.js
@@ -78,7 +78,8 @@ router.get('/', function(req, res, next){
  *  Return all jobs I posted
  */
 router.get('/:uerId/post', function(req, res, next) {
-    let query = {"owner": ObjectId(req.params['userId'])};
+    let query = {"owner": req.params['userId']};
+    console.log("Samuel test query = " + query);
     req.jobs.find(query).sort("preferDate", 1).toArray(function(err, docArray){
         if (err) next(err);
         res.json(docArray);
--- swiftHire-Web/src/app/app.routes.ts
+++ swiftHire-Web/src/app/app.routes.ts
@@ -3,15 +3,21 @@ import { JobsComponent } from "./jobs/jobs.component";
 import { ErrorComponent } from "./error/error.component";
 import { CandidatesComponent } from './component/candidates.component';
 import { CandidateComponent } from './component/candidate.component';
+import { JobsPostedComponent } from './component/jobsposted.component';
+
 
 const MY_ROUTES: Routes = [
-    { path: '', redirectTo: 'jobs', pathMatch: 'full' }, 
+    { path: '', redirectTo: 'jobs', pathMatch: 'full' },
     { path: 'jobs', component: JobsComponent },
      { path: 'error', component: ErrorComponent },
      // A redirect route requires a pathMatch property to tell the router how to match a URL to the path of a route.
      // The default matching strategy of Angular router is
      // to match the redirect route when the URL begins with the redirect route's prefix path.
      // if 'prefix' all params/routes/xx will be redirected to homepage
+
+    { path: 'jobs/posted', component: JobsPostedComponent, children:[
+
+    ]},
 
      { path: 'candidates', component: CandidatesComponent, children:[
           {path: 'detail', component: CandidateComponent}
